ffc6f6ac1ba248db51520dfc220af1da19e2d70e
leod-e1 <leodangeli11@gmail.com>
2022-12-30 19:53:59 -0300

    [update]change doughnut chart measure

--- src/view/Dashboard.jsx
+++ src/view/Dashboard.jsx
@@ -7,8 +7,8 @@ const Dashboard = () => {
 	return (
 		<div className='w-full'>
 			<ResumeCards />
-			<div className='grid grid-cols-1 lg:grid-cols-5 gap-4 lg:gap-8 my-3'>
-				<div className='lg:col-span-3'></div>
+			<div className='grid grid-cols-1 lg:grid-cols-4 gap-4 lg:gap-8 my-3'>
+				<div className='lg:col-span-2'></div>
 				<div className='lg:col-span-2'>
 					<Chart />
 				</div>
