606afb1b736257153f8676085a7ce32d8079539f
leod-e1 <leodangeli11@gmail.com>
2023-01-04 17:53:35 -0300

    [update] added cross in modal to close it

--- src/components/Content/Modal.jsx
+++ src/components/Content/Modal.jsx
@@ -1,14 +1,25 @@
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useModalStore } from '../../store/modalStore';
 import CreateProduct from '../Dashboard/e-commerce/products/CreateProduct';
 
 const Modal = ({ modalType }) => {
+	const updateVisibility = useModalStore(state => state.updateVisibility);
 	const modals = {
 		'create-product': CreateProduct,
 	};
 
 	return (
 		<div className='absolute z-20 bg-gray-dark bg-opacity-70 flex justify-center items-center w-full h-screen'>
-			{modals[modalType]()}
+			<div className='relative p-5'>
+				<FontAwesomeIcon
+					className='absolute top-0 -right-3 h-5 w-5 hover:bg-white p-2 rounded-full'
+					icon={faXmark}
+					onClick={() => updateVisibility(false)}
+				/>
+				{modals[modalType]()}
+			</div>
 		</div>
 	);
 };
