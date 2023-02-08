3a76ff356bbbc76429a1fd473f3d80b507c67ad6
leod-e1 <leodangeli11@gmail.com>
2023-02-07 23:39:45 -0300

    feat: update navbar style

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,10 +1,10 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { useUserStore } from '../../store/userStore';
 
 const Navbar = () => {
-	const [links] = useState([
+	const links = [
 		{
 			id: '0',
 			title: 'Home',
@@ -30,34 +30,40 @@ const Navbar = () => {
 			title: 'Dashboard',
 			url: '/dashboard',
 		},
-	]);
+	];
+
+	const user = useUserStore(state => state.user);
 
 	return (
 		<>
-			<header className='z-10 absolute w-full bg-transparent text-white font-kanitExtraBold font- text-xl'>
+			<header className='z-10 absolute w-full bg-transparent bg-opacity-20 bg-black text-white'>
 				<div className='mx-auto flex max-w-7xl items-center justify-between p-4'>
 					<a href='#'>
 						<h1 className='text-3xl'>Logo</h1>
 					</a>
-					<nav className='hidden items-center space-x-2 text-md font-medium text-gray-800 md:flex'>
+					<nav className='hidden items-center text-md md:flex'>
 						{links.map(link => (
-							<a
+							<Link
 								key={link.id}
-								href={link.url}
-								className='p-3 transition hover:bg-gray-100'
+								to={link.url}
+								className='p-3 transition hover:bg-gray-100 font-kanit'
 							>
 								{link.title}
-							</a>
+							</Link>
 						))}
 					</nav>
-					<nav className='hidden md:flex items-center text-md text-gray-800'>
-						<Link to='/login' className=' p-3 transition hover:bg-gray-400'>
-							Login
-						</Link>
-						<Link to='/signup' className='p-3 transition hover:bg-gray-400'>
-							Sign Up
-						</Link>
-					</nav>
+					{!user ? (
+						<nav className='hidden md:flex items-center text-md text-gray-800'>
+							<Link to='/login' className=' p-3 transition hover:bg-gray-400'>
+								Login
+							</Link>
+							<Link to='/signup' className='p-3 transition hover:bg-gray-400'>
+								Sign Up
+							</Link>
+						</nav>
+					) : (
+						<span className='text-white'>{user.email}</span>
+					)}
 					<div className='flex items-center space-x-2'>
 						<button className='flex appearance-none p-1 text-gray-500 md:hidden'>
 							<FontAwesomeIcon icon={faBars} className='h-5 w-5 text-white' />
